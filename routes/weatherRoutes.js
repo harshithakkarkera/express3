651d474afbc33381d4b9ec888110348e3077c1c4
harshithakkarkera <154577802+harshithakkarkera@users.noreply.github.com>
2024-12-05 10:46:18 +0530

    Update weatherRoutes.js

--- routes/weatherRoutes.js
+++ routes/weatherRoutes.js
@@ -4,5 +4,7 @@ const weatherCont =require('./../weatherController')
 const router =express.Router()
 router.route("/").get(weatherCont.getAllCity).post(weatherCont.addCity)
 router.route("/raindetails").get(weatherCont.getRaindetails)
+router.route("/add").post(weatherCont.add)
+router.route("/getcitys").get(authMiddleware.authenticateToken,weatherCont.getdetails)
 router.route("/city/:name").get(weatherCont.getCityWeather).delete(weatherCont.deleteCity).put(weatherCont.updateRainDetails);
 module.exports= router
